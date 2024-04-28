817a8568b61221a53dccc70d7baffceaabbc4730
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-04-27 18:51:43 -0600

    add login component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,8 @@ import Footer from "./Footer/Footer";
 import Container from "./container/container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Button from "./Button";
+import Login from "./Login";
 
 export { Header, Footer, Container, Logo, LogoutBtn };
