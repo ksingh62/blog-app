81c7904aa3c85930203290307b6bacbfa8352db7
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-04-28 04:13:47 -0600

    add pages and routing

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,31 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/container";
+import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Input from "./Input";
 import Button from "./Button";
-import Login from "./Login";
 import RTE from "./RTE";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Select from "./Select";
 import Signup from './Signup'
 
-export { Header, Footer, Container, Logo, LogoutBtn, Input, Button, Login, RTE, Signup };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Input,
+  Button,
+  RTE,
+  Signup,
+  Login,
+  PostForm,
+  PostCard,
+  AuthLayout,
+  Select
+};
