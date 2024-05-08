bb8a95febc8dbbd8c38a736a292ef040cdc0f0f6
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-05-08 13:31:19 -0600

    bug fix: nav item button not focused on refresh or pageload

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -27,7 +27,7 @@ function Footer() {
               <ul>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium hover:text-gray-400 text-[#EFD9CE]"
+                    className=" text-base font-medium hover:text-gray-400 text-gray-200"
                     to="/"
                   >
                     Features
@@ -35,7 +35,7 @@ function Footer() {
                 </li>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-[#EFD9CE] hover:text-gray-400"
+                    className=" text-base font-medium text-gray-200 hover:text-gray-400"
                     to="/"
                   >
                     Pricing
@@ -43,7 +43,7 @@ function Footer() {
                 </li>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-[#EFD9CE] hover:text-gray-400"
+                    className=" text-base font-medium text-gray-200 hover:text-gray-400"
                     to="/"
                   >
                     Affiliate Program
@@ -51,7 +51,7 @@ function Footer() {
                 </li>
                 <li>
                   <Link
-                    className=" text-base font-medium text-[#EFD9CE] hover:text-gray-400"
+                    className=" text-base font-medium text-gray-200 hover:text-gray-400"
                     to="/"
                   >
                     Press Kit
@@ -68,7 +68,7 @@ function Footer() {
               <ul>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-[#EFD9CE] hover:text-gray-400"
+                    className=" text-base font-medium text-gray-200 hover:text-gray-400"
                     to="/"
                   >
                     Account
@@ -76,7 +76,7 @@ function Footer() {
                 </li>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-[#EFD9CE] hover:text-gray-400"
+                    className=" text-base font-medium text-gray-200 hover:text-gray-400"
                     to="/"
                   >
                     Help
@@ -84,7 +84,7 @@ function Footer() {
                 </li>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-[#EFD9CE] hover:text-gray-400"
+                    className=" text-base font-medium text-gray-200 hover:text-gray-400"
                     to="/"
                   >
                     Contact Us
@@ -92,7 +92,7 @@ function Footer() {
                 </li>
                 <li>
                   <Link
-                    className=" text-base font-medium text-[#EFD9CE] hover:text-gray-400"
+                    className=" text-base font-medium text-gray-200 hover:text-gray-400"
                     to="/"
                   >
                     Customer Support
@@ -109,7 +109,7 @@ function Footer() {
               <ul>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-[#EFD9CE] hover:text-gray-400"
+                    className=" text-base font-medium text-gray-200 hover:text-gray-400"
                     to="/"
                   >
                     Terms &amp; Conditions
@@ -117,7 +117,7 @@ function Footer() {
                 </li>
                 <li className="mb-4">
                   <Link
-                    className=" text-base font-medium text-[#EFD9CE] hover:text-gray-400"
+                    className=" text-base font-medium text-gray-200 hover:text-gray-400"
                     to="/"
                   >
                     Privacy Policy
@@ -125,7 +125,7 @@ function Footer() {
                 </li>
                 <li>
                   <Link
-                    className=" text-base font-medium text-[#EFD9CE] hover:text-gray-400"
+                    className=" text-base font-medium text-gray-200 hover:text-gray-400"
                     to="/"
                   >
                     Licensing
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Container, Logo, LogoutBtn } from "../index";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 // import './header.css'
 
 function Header() {
   const authStatus = useSelector((state) => state.auth.status);
   const navigate = useNavigate();
+  const location = useLocation();
+  console.log(location);
 
   const navItems = [
     {
@@ -36,6 +38,11 @@ function Header() {
       active: authStatus,
     },
   ];
+
+  const getNavItemClass = (slug) => {
+    return `inline-block px-6 py-2 duration-200 hover:bg-purple-200 rounded-full hover:text-[#1f1a24] ${location.pathname === slug ? 'bg-purple-800' : ''}`
+  }
+  
   return (
     <header className="py-3 shadow bg-[#131315] text-[#EFD9CE] border-b-2 border-gray-600">
       <Container>
@@ -52,7 +59,7 @@ function Header() {
                 <li key={item.name}>
                   <button
                     onClick={() => navigate(item.slug)}
-                    className="inline-block px-6 py-2 duration-200 hover:bg-purple-200 rounded-full hover:text-[#1f1a24] focus:bg-purple-800"
+                    className={getNavItemClass(item.slug)}
                   >
                     {item.name}
                   </button>
