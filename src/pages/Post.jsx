eca66df4cf882a239bce33b88d2c392f27090224
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-05-08 14:15:44 -0600

    edit css

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -33,7 +33,7 @@ export default function Post() {
   };
 
   return post ? (
-    <div className="py-8 bg-[#131315] text-gray-200">
+    <div className="py-8 bg-[#131315] text-gray-200 min-h-[80vh]">
       <Container>
         <div className="w-full flex justify-center mb-4 relative border rounded-xl p-2">
           <img
