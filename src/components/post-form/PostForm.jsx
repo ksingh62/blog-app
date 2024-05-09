80461bb314200e7ab8399b65a8d01e836af54a5b
Kapilmeet Singh <kapilmeetsingh15@gmail.com>
2024-05-08 18:16:01 -0600

    add posts page css

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -77,7 +77,7 @@ export default function PostForm({ post }) {
 
   return (
     <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
-      <div className="w-2/3 px-2">
+      <div className="px-2 w-full md:w-2/3">
         <Input
           label="Title :"
           placeholder="Title"
@@ -102,7 +102,7 @@ export default function PostForm({ post }) {
           defaultValue={getValues("content")}
         />
       </div>
-      <div className="w-1/3 px-2">
+      <div className="px-2 w-full md:w-1/3 ">
         <Input
           label="Featured Image :"
           type="file"
